022d9fa6d7b66da72889f0b22beb9c0aa59966ae
hugolca <hugolcabreu@gmail.com>
2024-12-21 17:41:55 -0300

    Classe modelo instituicao

---
+++ Projeto/src/modelo/Instituicao.java
@@ -0,0 +1,40 @@
+package modelo;
+
+public class Instituicao {
+	String codigoDoMec, nome, tipo;
+	int anoDeFundacao;
+	
+	public Instituicao(String codigoDoMec, String nome, String tipo, int anoDeFundacao) {
+		this.codigoDoMec = codigoDoMec;
+		this.nome = nome;
+		this.tipo = tipo;
+		this.anoDeFundacao = anoDeFundacao;
+	}
+	
+	public String getCodigoDoMec() {
+		return codigoDoMec;
+	}
+	public void setCodigoDoMec(String codigoDoMec) {
+		this.codigoDoMec = codigoDoMec;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public int getAnoDeFundacao() {
+		return anoDeFundacao;
+	}
+	public void setAnoDeFundacao(int anoDeFundacao) {
+		this.anoDeFundacao = anoDeFundacao;
+	}
+	
+	
+}
