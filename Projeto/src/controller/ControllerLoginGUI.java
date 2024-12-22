7ca3a3dd351fa8d435e8480bca1c2342718b6593
Victória Carvalhal <vtcarvalhal@gmail.com>
2024-12-22 14:11:00 -0300

    CriadoControllerLoginGUI

---
+++ Projeto/src/controller/ControllerLoginGUI.java
@@ -0,0 +1,11 @@
+package controller;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
+import view.LoginGUI;
+
+public class ControllerLoginGUI {
+	
+}
